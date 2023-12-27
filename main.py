744e158365ce55a99b6a1c674bfac03489cc5aab
Dylan John <dylansejohn@gmail.com>
2023-12-27 13:23:12 +0000

    Movement
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   README.MD
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import pygame as pg
 from random import randrange
 
-WINDOW = 800
+WINDOW = 1000
+TILE_SIZE = 50
+RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
+get_random_position = lambda: [randrange(*RANGE), randrange(*RANGE)]
+snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
+snake.center = get_random_position()
+length = 1
+segments = [(snake.copy())]
+snake_dir = (0, 0)
+time, time_step = 0, 128
 screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
 
@@ -9,6 +18,22 @@ while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_w:                       
+                snake_dir = (0, -TILE_SIZE)
+            if event.key == pg.K_s:
+                snake_dir = (0, TILE_SIZE)
+            if event.key == pg.K_a:
+                snake_dir = (-TILE_SIZE, 0)
+            if event.key == pg.K_d:
+                snake_dir = (TILE_SIZE, 0)
     screen.fill('black')
+    [pg.draw.rect(screen, 'green', segment) for segment in segments]
+    time_now = pg.time.get_ticks()
+    if time_now - time > time_step:
+        time = time_now
+        snake.move_ip(snake_dir)
+        segments.append(snake.copy())
+        segments = segments[-length:]
     pg.display.flip()
     clock.tick(60)
